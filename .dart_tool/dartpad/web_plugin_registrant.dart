8624722b408cd81a6c58be61fc7348b84310f3af
Alex <oleksandr.samoilenko@extrawest.com>
2024-01-17 16:17:35 +0200

    Excluded .idea from being tracked

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,14 +7,20 @@
 // ignore_for_file: type=lint
 
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   ConnectivityPlusWebPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   FlutterSecureStorageWeb.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
