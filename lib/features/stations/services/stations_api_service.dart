25975544b09be0a7c5bc525c886cc7f9bf6e927a
Alex <oleksandr.samoilenko@extrawest.com>
2024-01-31 17:31:46 +0200

    remove dead code

--- lib/features/stations/services/stations_api_service.dart
+++ lib/features/stations/services/stations_api_service.dart
@@ -3,7 +3,6 @@ import 'package:samoilenko_maps_app/features/stations/models/station.dart';
 import '../../../common/services/services.dart';
 import '../../../network/api_client.dart';
 
-const int defaultPostsLimit = 20;
 const String stationsPath = '/stations.json';
 
 abstract class StationsApiService {
