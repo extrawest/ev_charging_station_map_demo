93c7bf1401b02fef08b59180f0b7cc6960ab553a
Alex <oleksandr.samoilenko@extrawest.com>
2024-02-03 02:28:16 +0200

    refactor connector model

--- lib/features/stations/models/connector.dart
+++ lib/features/stations/models/connector.dart
@@ -3,6 +3,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'connector.g.dart';
 part 'connector.freezed.dart';
 
+
+enum ConnectorType {
+  TYPE_2_PLUG, CCS_COMBO_1_2, TYPE_1,
+}
 @freezed
 class Connector with _$Connector{
   const factory Connector({
@@ -12,7 +16,7 @@ class Connector with _$Connector{
     String? id,
     double? power,
     double? tariff,
-    String? type,
+    ConnectorType? type,
   }) = _Connector;
 
   factory Connector.fromJson(Map<String, dynamic> json) => _$ConnectorFromJson(json);
