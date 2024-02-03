99d973250f18d5239b82d964aacc433677b9b791
Alex <oleksandr.samoilenko@extrawest.com>
2024-02-03 02:51:35 +0200

    move widgets to separate features

--- lib/features/stations/models/station.dart
+++ lib/features/stations/models/station.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../utils/cluster_utils.dart';
+import '../../google_map/utils/cluster_utils.dart';
 import 'connector.dart';
 
 part 'station.freezed.dart';
