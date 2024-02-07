7cbaecc42d48285d93cd156f5c5e55397ed90cc8
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-02-07 20:26:16 +0200

    add date util

--- lib/common/utils/date_format_utils.dart
+++ lib/common/utils/date_format_utils.dart
@@ -7,3 +7,11 @@ String convertStringToDMYH(String rawDate) {
       DateFormat("dd MMMM yyyy, HH:mm").format(dateTime);
   return formattedDate;
 }
+
+String convertStringTOyyMMddHHmm(String rawDate) {
+  final DateTime dateTime = DateTime.parse(rawDate);
+
+  final String formattedDate = DateFormat('yy/MM/dd HH:mm').format(dateTime);
+
+  return formattedDate;
+}
