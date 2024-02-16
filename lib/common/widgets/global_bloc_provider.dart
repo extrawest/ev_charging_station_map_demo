67e2b8298e7e5df5b99474a0c5e0425b78b63d0d
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-02-16 19:25:19 +0200

    regenerate firebase files

--- lib/common/widgets/global_bloc_provider.dart
+++ lib/common/widgets/global_bloc_provider.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../features/google_map/map_bloc/map_pin_bloc/map_pin_cubit.dart';
 import '../../features/google_map/map_bloc/map_type_bloc/map_cubit.dart';
+import '../../features/profile/auth_bloc/auth_bloc.dart';
 import '../../features/theme/theme.dart';
 import '../services/theme_box.dart';
 
@@ -30,6 +31,7 @@ class GlobalBlocProvider extends StatelessWidget {
         BlocProvider<MapPinCubit>(
           create: (context) => MapPinCubit(),
         ),
+
 
       ],
       child: child,
