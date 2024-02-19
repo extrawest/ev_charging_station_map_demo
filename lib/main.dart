d92b8e6f05c14ad627eb7fb6b7b382bac2c47a33
Alex <oleksandr.samoilenko@extrawest.com>
2024-02-19 17:53:25 +0200

    init Firebase for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,36 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:samoilenko_maps_app/utils/platform_selector.dart';
+import 'package:universal_html/html.dart' as html;
+import 'package:universal_html/js.dart' as js;
 
 import 'application.dart';
 import 'common/assets.dart';
+import 'common/constants/firebase_constants.dart';
 import 'common/services/services.dart';
 import 'common/widgets/widgets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: firebaseOptionsValues,
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
 
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorage.webStorageDirectory
-        : await getApplicationDocumentsDirectory(),
+    storageDirectory: kIsWeb ? HydratedStorage.webStorageDirectory : await getApplicationDocumentsDirectory(),
   );
   if (kIsWeb) {
     PlatformSelector().createScriptElement();
+    js.context['DEFINE_WEB_KEY'] = const String.fromEnvironment('DEFINE_WEB_KEY');
+    html.document.dispatchEvent(html.CustomEvent("dart_loaded"));
   }
 
   await AppBootstrapper().bootstrap();
