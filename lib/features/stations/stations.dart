2418cf96b83599dfdfebafe1fc92dd722483ba9a
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-02-21 18:49:08 +0200

    add params to MapMarker model

--- lib/features/stations/stations.dart
+++ lib/features/stations/stations.dart
@@ -2,4 +2,5 @@ export 'bloc/stations_bloc/stations_bloc.dart';
 export 'models/models.dart';
 export 'repositories/repositories.dart';
 export 'screens/screens.dart';
+export 'utils/utils.dart';
 export 'widgets/widgets.dart';
