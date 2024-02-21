4cf74372d7708b8af3a785394e5181ea6a12dda7
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-02-21 18:47:01 +0200

    Station  model to hive notation

--- lib/features/stations/models/station.dart
+++ lib/features/stations/models/station.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
 
 import '../../google_map/utils/cluster_utils.dart';
 import 'connector.dart';
@@ -8,17 +9,19 @@ part 'station.freezed.dart';
 part 'station.g.dart';
 
 
+@HiveType(typeId: 1)
 @freezed
 class Station with _$Station {
+  @HiveField(0)
   const factory Station({
-    @JsonKey(name: 'chargePointId') String? chargePointId,
-    @JsonKey(name: 'connectors') List<Connector>? connectors,
-    @JsonKey(name: 'imageUrls') Map<String, List<String>>? imageUrls,
-    double? latitude,
-    double? longitude,
-    @JsonKey(name: 'stationId') String? stationId,
-    StatusValues? status,
-    @JsonKey(name: 'tenantId') String? tenantId,
+    @HiveField(1) @JsonKey(name: 'chargePointId') String? chargePointId,
+    @HiveField(2) @JsonKey(name: 'connectors') List<Connector>? connectors,
+    @HiveField(3) @JsonKey(name: 'imageUrls') Map<String, List<String>>? imageUrls,
+    @HiveField(4) double? latitude,
+    @HiveField(5) double? longitude,
+    @HiveField(6) @JsonKey(name: 'stationId') String? stationId,
+    @HiveField(7) StatusValues? status,
+    @HiveField(8) @JsonKey(name: 'tenantId') String? tenantId,
   }) = _Station;
 
   factory Station.fromJson(Map<String, Object?> json) => _$StationFromJson(json);
