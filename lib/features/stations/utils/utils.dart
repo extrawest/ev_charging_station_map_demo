7db60933c83c3fbb689cb88cff593da09c2be37a
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-02-22 15:49:36 +0200

    change firstWhere to any, fix analysis issues

--- lib/features/stations/utils/utils.dart
+++ lib/features/stations/utils/utils.dart
@@ -1,12 +1,1 @@
-import '../models/map_marker/map_marker.dart';
 
-bool isStationAddedToFavorites({
-  required List<CustomMapMarker> addedStations,
-  required String currentStationName,
-}) {
-  try {
-    addedStations.firstWhere((station) => station.name == currentStationName);
-    return true;
-  } catch (e) {
-    return false;
-  }}
